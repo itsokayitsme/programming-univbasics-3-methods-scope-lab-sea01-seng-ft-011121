5ac919b50079128ebb20f1229ceae5880c89b01f
Johnny Cao <jcao0928@gmail.com>
2020-12-22 12:15:44 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,3 +14,9 @@ def link
   puts phrase
 end
 
+def all_phrases
+  phrase = "It's-a me, Mario!" 
+  phrase = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
